1822ea393af6272d1c400d5fe49c3a99f1e7df56
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-23 14:34:30 +0100

    Cubit como Gestor de estado - Inicio de una app de conteo

---
+++ lib/presentation/bloc/counter_cubit/counter_cubit.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_state.dart';
+
+// ! Es como el caso del StateNotifier de Riverpod en la aplicación de los widgets. Cubit<> maneja la clase/estado que pongamos entre <>
+
+// !      Riverpod                           -->      Cubit
+// !      class A extends StateNotifier<B>            class A extends Cubit<B>
+
+// ! Donde la clase 'A' va a controlar el estado de la clase 'B'. Además en ambos casos, la clase B tiene que inicializarse con 'A(): super(B())'.
+// * CounterCubit() maneja el CounterState() a través de Cubit<>
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState( counter: 5)); //* Estamos obligados a crear un constructor que invoque/inicialice la clase padre de la que heredamos
+  // State = Estado = new CounterState(); --> Estamos creando una instancia de AppTheme
+
+  void increaseBy( int value ) {
+    // Una forma
+    // emit( CounterState(
+    //   counter: state.counter + value,
+    //   transactionCount: state.transactionCount + 1
+    // ));
+    
+    //* Mejor que lo anterior porque lo hemos definido de tal manera que no estamos obligados a enviar todos los parametros, 
+    //* mientras que con la otra opcion estamos creando CounterState() completo cada vez y obligados a definir cada parametro.
+    emit(state.copyWith(
+      counter: state.counter + value,
+      transactionCount: state.transactionCount + 1
+    ));
+  }
+
+  void reset() {
+    emit(state.copyWith(
+      counter: 0
+    ));
+  }
+
+}
---
+++ lib/presentation/bloc/counter_cubit/counter_state.dart
@@ -0,0 +1,36 @@
+part of 'counter_cubit.dart';
+
+// sealed class CounterState {} 
+// A sealed class cannot be extended outside of its library (file). This restriction allows the compiler to benefit from it.
+// https://medium.com/@aliammariraq/sealed-classes-in-dart-unlocking-powerful-features-d8dba185925f
+
+// final class CounterInitial extends CounterState {}
+
+// * Clase que define el estado (el cómo queremos que luzca la información)
+class CounterState {
+
+  final int counter;
+  final int transactionCount; // numero de veces que el contador ha cambiado de estado
+
+  // Una forma de hacerlo
+  // CounterState({
+  //   required this.counter, 
+  //   required this.transactionCount
+  // }); 
+
+  CounterState({
+    this.counter = 0, 
+    this.transactionCount = 0
+  }); 
+
+  // ! Es una forma de hacer una copia de clases (del CounterState actual en este caso)
+  //! De esta manera podemos mantener la clase (CounterState) inmutable y, cuando se quiera cambiar algo, se hace una copia de ella
+  CounterState copyWith({
+    int? counter, // ? == opcinales
+    int? transactionCount
+  }) => CounterState(
+    counter: counter ?? this.counter, // si no me mandan valor, usa el que aparece en CounterState por defecto
+    transactionCount: transactionCount ?? this.transactionCount // idem
+  );
+
+}
