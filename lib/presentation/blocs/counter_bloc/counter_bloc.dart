5b0154d4b8859f49846772dfa282b911742e8507
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-24 12:50:41 +0100

    Simplificar el handler del BLoC

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -6,12 +6,16 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
-    on<CounterIncreased>((event, emit) { //? Manejador / Handler de un CounterEvent
-
-      emit(state.copyWith(
-        counter: state.counter + event.value, //* event == CounterIncreased(), el cual tiene la property 'value'
-        transactionCount: state.transactionCount + 1
-      ));    
-    });
+    //* Manejador / Handler de un CounterEvent
+    on<CounterIncreased>((event, emit) => _onCounterIncreased(event, emit));
+    // Como los argumentos son los mismos que se envian, se sobreentiende y tambien seria valido mandarlos por referencia de la siguiente manera
+    // on<CounterIncreased>(_onCounterIncreased);
+  }
+  
+  void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
+    emit(state.copyWith(
+      counter: state.counter + event.value, //* event == CounterIncreased(), el cual tiene la property 'value'
+      transactionCount: state.transactionCount + 1
+    ));
   }
 }
